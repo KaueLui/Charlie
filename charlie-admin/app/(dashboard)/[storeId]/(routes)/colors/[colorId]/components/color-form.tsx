1356867d3b0ea28901d9e008d6666f6518310158
Louiis04 <luiseduardosport1000@gmail.com>
2024-08-26 01:09:28 -0300

    Charlie-ChangeUX(Colors)

--- charlie-admin/app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/color-form.tsx
+++ charlie-admin/app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/color-form.tsx
@@ -41,7 +41,7 @@ export const ColorForm: React.FC<ColorFormProps> = ({
     const action = initialData ? "Salvar mudanças" : "Criar"
 
     const [open, setOpen] = useState(false);
-    const [ loading, setLoading ] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const form = useForm<ColorFormValues>({
         resolver: zodResolver(formSchema),
@@ -54,17 +54,17 @@ export const ColorForm: React.FC<ColorFormProps> = ({
     const onSubmit = async (data: ColorFormValues) => {
         try {
             setLoading(true);
-            if (initialData){
-            await axios.patch(`/api/${params.storeId}/colors/${params.colorId}`, data);
-        } else {
-            await axios.post(`/api/${params.storeId}/colors`, data);
-        }
+            if (initialData) {
+                await axios.patch(`/api/${params.storeId}/colors/${params.colorId}`, data);
+            } else {
+                await axios.post(`/api/${params.storeId}/colors`, data);
+            }
             router.refresh();
             router.push(`/${params.storeId}/colors`);
             toast.success(toastMessage)
         } catch (error) {
             toast.error("Algo deu errado!")
-        } finally{
+        } finally {
             setLoading(false)
         }
     };
@@ -77,8 +77,8 @@ export const ColorForm: React.FC<ColorFormProps> = ({
             router.push(`/${params.storeId}/colors`)
             toast.success("Cor deletada.")
         } catch (error) {
-           toast.error("Tenha certeza de que removeu todas as categorias que usam estas cores primeiro.") 
-        } finally{
+            toast.error("Tenha certeza de que removeu todas as categorias que usam estas cores primeiro.")
+        } finally {
             setLoading(false)
             setOpen(false)
         }
@@ -86,7 +86,7 @@ export const ColorForm: React.FC<ColorFormProps> = ({
 
     return (
         <>
-            <AlertModal 
+            <AlertModal
                 isOpen={open}
                 onClose={() => setOpen(false)}
                 onConfirm={onDelete}
@@ -94,62 +94,76 @@ export const ColorForm: React.FC<ColorFormProps> = ({
             />
             <div className="flex items-center justify-between">
                 <Heading
-                title={title}
-                description={description}
+                    title={title}
+                    description={description}
                 />
 
-            {initialData && (
-                <Button
-                    disabled={loading}
-                    variant="destructive"
-                    size="icon"
-                    onClick={() => setOpen(true)}
-                >
-                    <Trash className="h-4 w-4" />
-                </Button>
-            )}
+                {initialData && (
+                    <Button
+                        disabled={loading}
+                        variant="destructive"
+                        size="icon"
+                        onClick={() => setOpen(true)}
+                    >
+                        <Trash className="h-4 w-4" />
+                    </Button>
+                )}
 
             </div>
             <Separator />
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
                     <div className="grid grid-cols-3 gap-8">
-                        <FormField 
-                        control={form.control}
-                        name="name"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Nome
-                                </FormLabel>
-                                <FormControl>
-                                    <Input disabled={loading} placeholder="Nome da Cor" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Nome
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input disabled={loading} placeholder="Nome da Cor" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
                             )}
                         />
-                        <FormField 
-                        control={form.control}
-                        name="value"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Value
-                                </FormLabel>
-                                <FormControl>
-                                    <div className="flex items-center gap-x-4">
-                                        <Input disabled={loading} placeholder="Valor da Cor" {...field} />
-                                        <div
-                                        className="border p-4 rounded-full"
-                                        style={{backgroundColor: field.value}}
-                                         />
-                                    </div>
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
+                        <FormField
+                            control={form.control}
+                            name="value"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Selecione a cor</FormLabel>
+                                    <FormControl>
+                                        <div className="flex items-center gap-x-4">
+                                            <div className="relative">
+                                                <input
+                                                    type="color"
+                                                    disabled={loading}
+                                                    {...field}
+                                                    className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                                                />
+                                                <button
+                                                    type="button"
+                                                    disabled={loading}
+                                                    className="border p-2 rounded-full focus:outline-none bg-gray-200" >
+                                                    <span className="material-icons text-black font-bold">V</span>
+                                                </button>
+                                            </div>
+                                            <div
+                                                className="border p-4 rounded-full"
+                                                style={{ backgroundColor: field.value }}
+                                            />
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
                             )}
                         />
+
+
+
                     </div>
                     <Button disabled={loading} className="ml-auto" type="submit">
                         {action}
